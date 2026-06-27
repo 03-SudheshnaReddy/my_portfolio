f448c21d2b8b04ca40718d5539ebfd501b4ffe49
Sudheshna Reddy <170701510+03-SudheshnaReddy@users.noreply.github.com>
2026-06-27 16:19:33 +0530

    removed space before headings

--- Portfolio-main/src/hoc/SectionWrapper.jsx
+++ Portfolio-main/src/hoc/SectionWrapper.jsx
@@ -5,13 +5,18 @@ import { staggerContainer } from "../utils/motion";
 
 const SectionWrapper = (Component, idName) =>
   function HOC() {
+    const isAbout = idName === "about";
+    const paddingClass = isAbout
+      ? "pt-24 sm:pt-36 pb-8 sm:pb-12 px-6 sm:px-16"
+      : "pt-6 pb-8 sm:pt-10 sm:pb-12 px-6 sm:px-16";
+
     return (
       <motion.section
         variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
         viewport={{ once: true, amount: 0.25 }}
-        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+        className={`${paddingClass} max-w-7xl mx-auto relative z-0`}
       >
         <span id={idName}>
           &nbsp;
